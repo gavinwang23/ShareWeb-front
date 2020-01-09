5cfa52273dda9b37878e502fabae5c7c6c11f5f7
18626548379 <2948216998@qq.com>
2020-01-09 14:26:27 +0800

    关于我们雏形

--- src/views/aboutus.vue
+++ src/views/aboutus.vue
@@ -1,7 +1,44 @@
 <template>
-<div>
-    <img src="../assets/picture/day.png" style="height:50ch;width:80vw;margin-left:10vw" />
-</div>    
+<el-container>
+<div>  
+    <div class="head">
+        图案填充，暂时未定图案
+    </div>
+    <div class="webstie">
+        <p class="webstiechinese">网站简介</p>
+        <p class="webstieenglish">Website introduction</p>
+        <div class="introductionneir">
+出国留学网（www.liuxue86.com），创立于2005年，隶属于长沙爱登网络科技有限公司，2013年开始公司化运营，凭借创始人团队多年积累的行业经验以及对互联网的深刻洞察，务实工作，砥砺前行，经历四年的高速发展，现已成为全国最大，最全的留学/移民资讯服务平台。
+2017年，公司正式启动品牌化战略，大力开展软硬实力的同步建设工作，以优质内容运营为依托，夯实基础，布局新媒体矩阵，多管齐下，探索高附加值的专业化内容服务，强化网站的品牌影响力，提升网站综合价值，全面打造留学移民领域的互联网第一品牌。
+本着一切以用户价值为导向的服务理念，以提供优质留学、移民资讯为基础，以探索最优化的留学/移民解决方案为使命，锐意进取，勇于创新，愿引领中国留学/移民产业不断创新与发展！
+        </div>
+    </div>
+    <div class="webstie">
+       <p class="webstiechinese">团队成员</p>
+        <p class="Teammembers"> Team members</p>
+    </div>    
+    <div class="personalnameone">
+        <img src="../assets/picture/aboutus.png" style="height:20vh;width:13vw;border:solid;border-color:black;float:left;">
+             <div>
+                  姓名：XXX
+             </div> 
+             <div>
+                 个人能力强，有经验，操作能力高,毕业于某某某大学，在学习期间经常废寝忘食的学习
+             </div>
+    </div>
+    <div class="personalnametow">
+             <div>
+                  姓名：XXX
+             </div> 
+             <div class="personaltext">
+                 个人能力强，有经验，操作能力高,毕业于某某某大学，在学习期间经常废寝忘食的学习个人能力强，有经验，操作能力高,毕业于某某某大学，在学习期间经常废寝忘食的学习
+             </div>
+             <img src="../assets/picture/aboutus.png" style="height:20vh;width:13vw;border:solid;border-color:black;float:right;margin-top:-20vh;">
+    </div>
+    <div class="foot">
+    </div>    
+</div>
+</el-container>
 </template>
 
 <style lang="scss" scoped>
