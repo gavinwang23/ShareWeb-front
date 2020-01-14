69bea2d09cf71e6fdf7f557ba54ef6b84f1fe3bf
18626548379 <2948216998@qq.com>
2020-01-14 10:42:19 +0800

    m评论功能初步改动

--- src/views/article.vue
+++ src/views/article.vue
@@ -11,8 +11,8 @@
       <div class="comment_area">
         <h1>评论区</h1>
         <div class="input">
-          <textarea></textarea>
-          <button>提交评论</button>
+          <textarea class="textareabox" @focus="submissionbutton=true" @blur="submissionbutton=fasle"></textarea>
+          <button class="submissionbutton" v-show="submissionbutton">发布</button>
         </div>
       </div>
     </el-main>
@@ -48,7 +48,8 @@ export default {
     return {
       title: "文章题目",
       content:
-        '<h3>很久很久以前</h3> <p>有个补碗匠叫赵光义。赵光义是补碗匠中的高手。</p> <img src="https://gss3.bdstatic.com/7Po3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=ac04969241a98226accc2375ebebd264/cb8065380cd79123d01d8093af345982b3b78053.jpg"><p>这天，赵光义挑着工具担子，刚走进一条胡同，便被一个人拽进一处院子里。这个人叫王直，他...</p>'
+        '<h3>很久很久以前</h3> <p>有个补碗匠叫赵光义。赵光义是补碗匠中的高手。</p> <img src="https://gss3.bdstatic.com/7Po3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=ac04969241a98226accc2375ebebd264/cb8065380cd79123d01d8093af345982b3b78053.jpg"><p>这天，赵光义挑着工具担子，刚走进一条胡同，便被一个人拽进一处院子里。这个人叫王直，他...</p>',
+      submissionbutton:false,    
     };
   },
   methods: {
