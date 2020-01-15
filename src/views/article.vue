567d4663fdfd039857b8acfccdba42413da55264
18626548379 <2948216998@qq.com>
2020-01-15 09:56:33 +0800

    评论区雏形

--- src/views/article.vue
+++ src/views/article.vue
@@ -7,8 +7,21 @@
       <div class="article_main">
         <h1>{{ title }}</h1>
         <div v-html="content"></div>
+        <el-button class="buttonstyle">赞</el-button>
+        <el-button class="buttonstyle">踩</el-button>
+        <el-button class="buttonstyle" @click="translate=true,hide=true">评论</el-button>
+        <el-button class="buttonstyle">分享</el-button>
+        <el-button class="buttonstyle">感兴趣</el-button>
+        <el-popover width="5vw" trigger="click">
+          <div>
+            <el-button>不感兴趣</el-button>
+            <el-button>举报</el-button>
+          </div>
+          <el-button class="buttonstyle" slot="reference">更多</el-button>
+        </el-popover>
+        <el-button v-show="hide" @click="translate=false,hide=false" class="hidebutton">收起评论</el-button>
       </div>
-      <div class="comment_area">
+      <div v-show="translate" class="comment_area">
         <h1>评论区</h1>
         <div class="input">
           <textarea class="textareabox" @focus="submissionbutton=true" @blur="submissionbutton=fasle"></textarea>
@@ -49,7 +62,9 @@ export default {
       title: "文章题目",
       content:
         '<h3>很久很久以前</h3> <p>有个补碗匠叫赵光义。赵光义是补碗匠中的高手。</p> <img src="https://gss3.bdstatic.com/7Po3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=ac04969241a98226accc2375ebebd264/cb8065380cd79123d01d8093af345982b3b78053.jpg"><p>这天，赵光义挑着工具担子，刚走进一条胡同，便被一个人拽进一处院子里。这个人叫王直，他...</p>',
-      submissionbutton:false,    
+      submissionbutton:false, 
+      translate:false,
+      hide:false,   
     };
   },
   methods: {
