00412d7faeeda66315342aa96f808ca61ee1d0d1
18626548379 <2948216998@qq.com>
2020-01-18 13:34:20 +0800

    评论条组件

---
+++ src/components/commentbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="value in list" :key="value.list" style="list-style-type: none;" class="peripheral">
+                <div class="usernamebox">
+                   {{value.userName}}：
+                </div>
+                <div class="commentbox">
+                    {{value.comment}}
+                </div>
+                <el-button class="goodbutton">
+                    <img src="../assets/icon/good.png" style="height:2vh;width:1vw"> {{value.number}}
+                </el-button>
+            </li>
+        </ul>
+    </div>
+</template>
+<style lang="scss" scoped>
+@import "../assets/css/components/commentbar.scss";
+</style>
+<script>
+export default {
+    data(){
+        return{
+            list:[
+                {
+                userName:"11111",
+                comment:"很久很久以前有个补碗匠叫赵光义。赵光义是补碗匠中的高手。这天，赵光义挑着工具担子，刚走进一条胡同，便被一个人拽进一处院子里。这个人叫王直，他...",
+                number:"23",
+                },
+                {
+                userName:"11111",
+                comment:"很久很久以前有个补碗匠叫赵光义。赵光义是补碗匠中的高手。这天，赵光义挑着工具担子，刚走进一条胡同，便被一个人拽进一处院子里。这个人叫王直，他...",
+                number:"23",
+                },
+            ]
+        };
+    }
+}
+</script>
--- src/views/article.vue
+++ src/views/article.vue
@@ -23,14 +23,8 @@
       </div>
       <div v-show="translate" class="comment_area">
         <h1>评论区</h1>
-          <img src="../assets/icon/notcomment.png" style="height:12vh;width:6vw;margin-left:25vw"><div class="notcomment">暂无评论</div>
-          <!-- <div class="commentarea"> 
-          <div class="commentbox">
-          <div class="username">11111:</div>
-          <div class="commentcontent">有个补碗匠叫赵光义。赵光义是补碗匠中的高手这天，赵光义挑着工具担子，刚走进一条胡同，便被一个人拽进一处院子里。这个人叫王直，他</div>
-          <el-button class="commentgood"><img src="../assets/icon/good.png" style="height:60%;width:40%">赞</el-button>
-          </div>
-          </div> -->
+          <!-- <img src="../assets/icon/notcomment.png" style="height:12vh;width:6vw;margin-left:25vw"><div class="notcomment">暂无评论</div> -->
+          <commentbar></commentbar>
         <div class="input">
           <!-- <textarea class="textareabox" @focus="submissionbutton=true" @blur="submissionbutton=fasle"></textarea> -->
           <wangEditor commentvalue="写评论" height="20vh" message="发布" editor="comment" class="wangEditorbox" @focus="submissionbutton=true" @blur="submissionbutton=fasle"></wangEditor>
@@ -50,10 +44,12 @@
 <script>
 import myHead from "./../components/header/header.vue";
 import wangEditor from "../components/wangEditor.vue";
+import commentbar from "../components/commentbar.vue"
 export default {
   components: {
     myHead,
-    wangEditor
+    wangEditor,
+    commentbar
   },
   created() {
     let userName = this.$route.params.userName;
